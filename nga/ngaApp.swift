d0efa1799e26d761dcd45f49725b7566b963946f
vivian <luckytezuka@gmail.com>
2026-06-17 03:53:20 +0800

    Initial Commit

---
+++ nga/ngaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ngaApp.swift
+//  nga
+//
+//  Created by CJ on 2026/6/17.
+//
+
+import SwiftUI
+
+@main
+struct ngaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
